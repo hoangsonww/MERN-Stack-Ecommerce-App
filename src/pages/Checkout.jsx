e8b1c471943dd9ac895b72b00404ca681f0542ac
Son Nguyen <hoangson091104@gmail.com>
2024-06-26 11:33:43 +0700

    Update: Added project documentation

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
 import CheckoutForm from '../components/CheckoutForm';
 import { Typography, CircularProgress } from '@mui/material';
 
