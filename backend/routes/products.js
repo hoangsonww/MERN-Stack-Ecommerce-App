1529129bb5c5b17c428b671cc2b4963f848fffaa
Son Nguyen <hoangson091104@gmail.com>
2024-09-05 13:57:46 -0400

    Update: Added nodemon and SwaggerJS

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -15,17 +15,46 @@ const Product = require('../models/product');
  *         name:
  *           type: string
  *           description: The name of the product
+ *         description:
+ *           type: string
+ *           description: The description of the product
  *         price:
  *           type: number
  *           description: The price of the product
  *         category:
  *           type: string
  *           description: The category of the product
+ *         image:
+ *           type: string
+ *           description: The image URL of the product
+ *         brand:
+ *           type: string
+ *           description: The brand of the product
+ *         stock:
+ *           type: number
+ *           description: The available stock of the product
+ *         rating:
+ *           type: number
+ *           description: The rating of the product
+ *         numReviews:
+ *           type: number
+ *           description: The number of reviews of the product
+ *         createdAt:
+ *           type: string
+ *           format: date-time
+ *           description: The date and time the product was created
  *       example:
  *         id: "66d9e7ee8bf3a567a5efe26b"
  *         name: "Product Name"
+ *         description: "Product Description"
  *         price: 19.99
  *         category: "Electronics"
+ *         image: "https://example.com/product.jpg"
+ *         brand: "Brand Name"
+ *         stock: 10
+ *         rating: 4.5
+ *         numReviews: 10
+ *         createdAt: "2022-01-01T00:00:00.000Z"
  */
 
 /**
