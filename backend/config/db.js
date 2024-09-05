f37ac9432c6155b3b677cbf5ac88ab2a0e6b9552
Son Nguyen <hoangson091104@gmail.com>
2024-09-05 18:09:35 -0400

    Update: Added nodemon and SwaggerJS

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
+// Connect to MongoDB
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
