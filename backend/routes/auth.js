097922b8c5852e55192acc9469a79a276f157da5
Son Nguyen <hoangson091104@gmail.com>
2024-11-01 15:45:52 -0400

    Update: Added nodemon and SwaggerJS

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -242,6 +242,7 @@ router.post(
   ],
   async (req, res) => {
     const errors = validationResult(req);
+
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
